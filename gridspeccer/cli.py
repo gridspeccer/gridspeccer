ef40e1d3a1efc1c19218b4a5d6298ab73e79395e
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2021-05-19 18:18:35 +0200

    loglevel as argument

--- gridspeccer/cli.py
+++ gridspeccer/cli.py
@@ -35,6 +35,12 @@ def plot():
         ),
     )
     parser.add_argument(
+        "--loglevel",
+        help="Display more or less info (values: DEBUG INFO WARNING ERROR)",
+        type=str,
+        default="INFO",
+    )
+    parser.add_argument(
         "--output-folder",
         help="Folder to output into.",
         type=str,
@@ -50,6 +56,8 @@ def plot():
         raise IOError(
             f"The 'mplrc' argument ('{args.mplrc}') has to be an existing file"
         )
+    log.setLevel(args.loglevel)
+
     mpl.rc_file(args.mplrc)
 
     if len(args.data) == 0:
